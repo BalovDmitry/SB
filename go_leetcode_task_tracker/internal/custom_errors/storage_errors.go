616737b34f4989387e2bae1bee5726d08ce7777d
Balov Dmitry <balov.dimitri@yandex.ru>
2025-07-23 10:00:33 +0300

    Added new custom errors.

--- go_leetcode_task_tracker/internal/custom_errors/storage_errors.go
+++ go_leetcode_task_tracker/internal/custom_errors/storage_errors.go
@@ -11,3 +11,15 @@ type TaskNotFoundError struct{}
 func (e TaskNotFoundError) Error() string {
 	return "task is not found"
 }
+
+type InitHandlerError struct{}
+
+func (e InitHandlerError) Error() string {
+	return "failed to initialize handler"
+}
+
+type RequestParserError struct{}
+
+func (e RequestParserError) Error() string {
+	return "can't receive data from request"
+}
